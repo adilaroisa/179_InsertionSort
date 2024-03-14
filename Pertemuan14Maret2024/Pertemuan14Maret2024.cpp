007ea73477b3a1e39ba34e4b9cdd0de7dbe97794
adilaroisa <adilaroisa688@gmail.com>
2024-03-14 12:32:27 +0700

    Membuat prosedur input

--- Pertemuan14Maret2024/Pertemuan14Maret2024.cpp
+++ Pertemuan14Maret2024/Pertemuan14Maret2024.cpp
@@ -4,6 +4,33 @@ using namespace std;
 int arr[20];    // Memuat array dengan panjang data 20
 int n;          // Membuat variabel inputan n
 
+void input() {  // Procedure input
+    while (true)
+    {
+        cout << "Masukan Jumlah Data pada Array : "; // Membuat inputan jumlah element Array
+        cin >> n;                                    // Memanggil variabel inputan n
+
+        if (n <= 20) {
+            break;
+        }
+        else
+        {
+            cout << "\nArray yang anda masukan maksimal 20 Elemen.\n";  // Menampilkan pesan jika data lebih dari 20
+        }
+    }
+    cout << endl;                                           // Membuat jarak perbaris program
+    cout << "======================" << endl;               // Membuat tampilan susunan data elemen array
+    cout << "Masukan Elemen Array" << endl;
+    cout << "======================" << endl;
+
+
+    for (int i = 0; i < n; i++)                             // Menggunakan peruangan for untuk menyimpan data array
+    {
+        cout << "Data ke-" << (i + 1) << ": ";              // Memasukan atau menginput nilai data n
+        cin >> arr[i];                                      // Menyimpan nilai data n ke dalam array arr
+    }
+}
+
 int main()
 {
    
