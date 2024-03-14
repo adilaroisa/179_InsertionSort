d8f92e3aa32845216a8c3745360ced80de87f6c5
adilaroisa <adilaroisa688@gmail.com>
2024-03-14 12:34:11 +0700

    Membuat prosedur menampilkan hasil

--- Pertemuan14Maret2024/Pertemuan14Maret2024.cpp
+++ Pertemuan14Maret2024/Pertemuan14Maret2024.cpp
@@ -60,6 +60,17 @@ void insertionsort() { // Procedure insertionsort
 
 }
 
+void display() {         // Procedure untuk menampilkan hasil
+    cout << endl;        // Output baris kosong
+    cout << "\n======================" << endl; // output ke layar
+    cout << "Elemen Array yang telah tersusun" << endl; // output ke layar
+    cout << "======================" << endl;           // output ke layar
+
+    for (int j = 0; j < n; j++) {    // Looping dengan j dimulai dari o hingga n-1
+        cout << arr[j] << endl;      // output ke layar
+    }
+    cout << endl;                    // output baris kosong
+}
 
 int main()
 {
